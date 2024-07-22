b4f0e49680368beaa3bc843a09ca54ea83344330
oRAIHANo <mraihanps.work@gmail.com>
2024-07-22 12:30:55 +0530

    added xyz

--- 2 DATA STRUCTURE/1_Data_Structure.c
+++ 2 DATA STRUCTURE/1_Data_Structure.c
@@ -1,5 +1,5 @@
 /*
-QWERTY
+afsdfsdfsdfas
 A data structure is a storage that is used to store and organize data. 
 It is a way of arranging data on a computer so that it can be accessed and updated efficiently.
 A data structure is not only used for organizing the data. It is also used for processing, retrieving, and storing data.
